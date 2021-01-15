4a4793ef784da0378467d5c15168bc7725578550
gentianamejzini <75703142+gentianamejzini@users.noreply.github.com>
2021-01-15 22:50:17 +0100

    Update game.js

--- game.js
+++ game.js
@@ -47,23 +47,3 @@ const reset = function(){
 	document.getElementById('form-user').reset();
 }
 document.addEventListener('DOMContentLoaded', init);
-
-
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
-
-
-
-
-
-
-
